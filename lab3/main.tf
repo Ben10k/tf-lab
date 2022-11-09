7b1a037368c48a540f8fac57fb629b3f65699343
benas.taurosevicius <benas.taurosevicius@mambu.com>
2022-11-10 01:08:03 +0200

    lab3 update lab4 add

--- lab3/main.tf
+++ lab3/main.tf
@@ -14,16 +14,3 @@ module "network" {
   prefix   = var.prefix
   rg_name  = azurerm_resource_group.rg.name
 }
-
-
-module "linuxservers" {
-  source = "Azure/compute/azurerm"
-
-  resource_group_name = azurerm_resource_group.rg.name
-  vm_os_simple        = "UbuntuServer"
-  vnet_subnet_id      = module.network.subnet_id
-  vm_size             = "Standard_B1ls"
-  nb_instances        = var.vm_count
-
-  depends_on = [azurerm_resource_group.rg]
-}
---
+++ lab4/install-terragrunt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p ~/.local/bin
+
+wget -O ~/.local/bin/terragrunt \
+ https://github.com/gruntwork-io/terragrunt/releases/download/v0.40.1/terragrunt_linux_amd64
+
+chmod +x ~/.local/bin/terragrunt
